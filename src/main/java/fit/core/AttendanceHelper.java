2e0e9a69378cb18053839750e9c5452685fadc19
Cleber dos Santos Vieira <cleber.vieira@bradesco.com.br>
2021-02-03 22:13:27 -0300

    teste chamada alunos

---
+++ src/main/java/fit/core/AttendanceHelper.java
@@ -0,0 +1,19 @@
+package fit.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AttendanceHelper {
+
+	public static Map<String, Boolean> random(int qtdAlunos, int percentual) {
+		Map<String, Boolean> lista = new HashMap<String, Boolean>();
+		
+		int bool = qtdAlunos * percentual / 100;
+		for(int x = 0; x < qtdAlunos; x++) {
+			lista.put(String.valueOf(x), x < bool ? true: false);
+		}
+		
+		return lista;
+	}
+
+}
